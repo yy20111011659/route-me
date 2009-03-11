e0c49da65e1abb90ce07b9c4a6c74267fd18790c
halmueller <halmueller@1b297cfb-d155-0410-8e73-0b86fed47e86>
2009-03-11 06:10:13 +0000

    tagged r287, final release to contain SimpleMap sample within framework project

---
+++ tags/final-version-with-self-contained-sample/samples/SampleMap/Classes/Sample2AppDelegate.m
@@ -0,0 +1,30 @@
+//
+//  Sample2AppDelegate.m
+//  Sample2 : Diagnostic map
+//
+
+#import "Sample2AppDelegate.h"
+#import "RootViewController.h"
+
+@implementation Sample2AppDelegate
+
+
+@synthesize window;
+@synthesize rootViewController;
+@synthesize mapContents;
+
+- (void)applicationDidFinishLaunching:(UIApplication *)application {
+    
+    [window addSubview:[rootViewController view]];
+    [window makeKeyAndVisible];
+}
+
+
+- (void)dealloc {
+    self.mapContents = nil;
+    [rootViewController release];
+    [window release];
+    [super dealloc];
+}
+
+@end
